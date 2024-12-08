9f3260e8388ae2516968a25908fb26e826cf17b1
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2024-12-08 20:05:54 +0900

    5.html 업데이트 및 통합 자바스크립트 관리 오류 수정

--- script/script.js
+++ script/script.js
@@ -1,37 +1,140 @@
-// index.html에 선언된 스크립트
+document.addEventListener('DOMContentLoaded', () => {
 
-$(() => {
-    $('#win_container').draggable();
-})
+
+    const currentPage = window.location.pathname;
+
+
+
+    /*====================index.html====================*/
+    if (currentPage.includes('index.html')) {
+
+        $(() => {
+            $('#win_container').draggable();
+        })
 
 
 //index.html에서 각 아이콘 클릭시 화면전환 기능 구현
-const icons = document.querySelectorAll('.iconFlex');  //총 9개의 iconFlex 클래스를 querySelectorAll 배열로 변수 지정
-const popupBox = document.getElementById('popup');  //아이콘 클릭시 자연스럽게 전환될 수 있도록 popup id 변수 지정
+        const icons = document.querySelectorAll('.iconFlex');  //총 9개의 iconFlex 클래스를 querySelectorAll 배열로 변수 지정
+        const popupBox = document.getElementById('popup');  //아이콘 클릭시 자연스럽게 전환될 수 있도록 popup id 변수 지정
 
-icons.forEach((icon,index) => {  //iconFlex div를 forEach, 클릭된 icon의 순서를 알기 위해 index를 추가로 선언
-    icon.addEventListener('click', () => {  //icon을 클릭하면
-        popupBox.classList.add('active');  //미리 선언해둔 팝업 변수에 active클래스를 추가하여 keyframe애니메이션 재생
+        icons.forEach((icon,index) => {  //iconFlex div를 forEach, 클릭된 icon의 순서를 알기 위해 index를 추가로 선언
+            icon.addEventListener('click', () => {  //icon을 클릭하면
+                popupBox.classList.add('active');  //미리 선언해둔 팝업 변수에 active클래스를 추가하여 keyframe애니메이션 재생
 
-        //핵심 코드. popupBox에 active 클래스가 추가되고 1초 후 내부 코드 실행
-        setTimeout(() => {
-            /*
-            * index페이지 전환 코드
-            * 각 서브 페이지 이름은 1~9.html로 지정해 두었음
-            * forEach에서 선언한 index값은 클릭한 icon의 순서
-            * querySelectorAll로 불러온 배열은 0부터 시작하므로 클릭한 index에 1을 더함
-            * 최종적으로 백틱을 사용하여 클릭한 icon에 대한 순서(index)값+1.html 파일을 1초 후 열도록 프로그래밍
-            * ex. 6번째 아이콘을 클릭 -> index값은 5 -> 5+1 => 6.html 1초 후 전환
-            * */
-            window.location.href = `./${index + 1}.html`;
-        }, 1000);
-    })
-})
+                //핵심 코드. popupBox에 active 클래스가 추가되고 1초 후 내부 코드 실행
+                setTimeout(() => {
+                    /*
+                    * index페이지 전환 코드
+                    * 각 서브 페이지 이름은 1~9.html로 지정해 두었음
+                    * forEach에서 선언한 index값은 클릭한 icon의 순서
+                    * querySelectorAll로 불러온 배열은 0부터 시작하므로 클릭한 index에 1을 더함
+                    * 최종적으로 백틱을 사용하여 클릭한 icon에 대한 순서(index)값+1.html 파일을 1초 후 열도록 프로그래밍
+                    * ex. 6번째 아이콘을 클릭 -> index값은 5 -> 5+1 => 6.html 1초 후 전환
+                    * */
+                    window.location.href = `./${index + 1}.html`;
+                }, 1000);
+            })
+        })
 
 
-const container = document.getElementById('win_container');
-const maxBtn = document.querySelector('.max');
+        const container = document.getElementById('win_container');
+        const maxBtn = document.querySelector('.max');
 
-maxBtn.addEventListener('click', () => {
-    container.classList.toggle('max');
-})
+        maxBtn.addEventListener('click', () => {
+            container.classList.toggle('max');
+        })
+
+    }
+
+
+
+
+
+
+
+    /*====================1.html====================*/
+
+
+
+
+
+
+    /*====================2.html====================*/
+
+
+
+
+
+
+    /*====================3.html====================*/
+
+
+
+
+
+
+    /*====================4.html====================*/
+
+
+
+
+
+
+    /*====================5.html====================*/
+    if (currentPage.includes('5.html')) {
+
+        const bezierControl = document.getElementById('bezier-control');
+        const bezierBtn = document.querySelector('.test-btn');
+
+        bezierBtn.addEventListener("click", () => {
+            // d 속성 변경
+            bezierControl.setAttribute(
+                "d",
+                "M10.5 327C43.5 327 132 99.5 214.5 99.5C335 99.5 332.5 326.5 839.5 326.5"
+            );
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+    /*====================6.html====================*/
+
+
+
+
+
+
+    /*====================7.html====================*/
+
+
+
+
+
+
+    /*====================8.html====================*/
+
+
+
+
+
+
+    /*====================9.html====================*/
+
+
+
+});
+
+
+
+
+
+
+
+
